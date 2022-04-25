e66dd70d69ff3db1018eb3e0013d21044a996b2c
KK44689 <61814979+KK44689@users.noreply.github.com>
2022-04-25 17:21:53 +0700

    add main menu style

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -5,7 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainUI : MonoBehaviour
 {
-    public void Restart(){
+    public static bool gameIsPause = false;
+
+    public GameObject PauseMenu;
+
+    public void Restart()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BacktoMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPause = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPause = false;
     }
 }
